6846e0a2c43fa792fc8248ec1c8a4d0b4ea667eb
Marcus Bernt <berntmarcus86@gmail.com>
2022-06-03 10:38:45 +0200

    [-] test > removed payment-mixin configuration > iways pp+ third party payment methods configured changed

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -8,9 +8,6 @@ var config = {
         mixins: {
             'Magento_Checkout/js/action/select-payment-method': {
                 'Compatibility_AmastyCheckoutIwaysPayPalPlus/js/action/select-payment-method-mixin': true
-            },
-            'Iways_PayPalPlus/js/view/payment/method-renderer/payment': {
-                'Compatibility_AmastyCheckoutIwaysPayPalPlus/js/view/payment/method-renderer/payment-mixin': true
             }
         }
     }
